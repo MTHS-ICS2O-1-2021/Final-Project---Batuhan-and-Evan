27bed8d04e732df90b90a97c50b77b4e0ee6d219
Batuhan Durhan <71403385+Batuhan-Durhan@users.noreply.github.com>
2022-06-15 17:04:57 +0000

    gamejs

--- js/game.js
+++ js/game.js
@@ -6,16 +6,16 @@
 // Created on: April 2022
 // This is the Phaser3 configuration file
 
-import SplashScene from "./splashScene.js"
-import TitleScene from "./titleScene.js"
-import MenuScene from "./menuScene.js"
-import GameScene from "./gameScene.js"
+import SplashScene from "./splashScene.js";
+import TitleScene from "./titleScene.js";
+import MenuScene from "./menuScene.js";
+import GameScene from "./gameScene.js";
 
 // Our game scenes
-const splashScene = new SplashScene()
-const titleScene = new TitleScene()
-const menuScene = new MenuScene()
-const gameScene = new GameScene()
+const splashScene = new SplashScene();
+const titleScene = new TitleScene();
+const menuScene = new MenuScene();
+const gameScene = new GameScene();
 
 //* Game Scenes */
 const config = {
@@ -37,15 +37,15 @@ const config = {
   },
 }
 
-const game = new Phaser.Game(config)
+const game = new Phaser.Game(config);
 // console.log(game)
 
 // load scenes
 // NOTE: remember any "key" is global and CAN NOT be reused!
-game.scene.add("splashScene", splashScene)
-game.scene.add("titleScene", titleScene)
-game.scene.add("menuScene", menuScene)
-game.scene.add("gameScene", gameScene)
+game.scene.add("splashScene", splashScene);
+game.scene.add("titleScene", titleScene);
+game.scene.add("menuScene", menuScene);
+game.scene.add("gameScene", gameScene);
 
 // the start scene
-game.scene.start("splashScene")
+game.scene.start("splashScene");
